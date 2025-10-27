1a0c9900ac15322d288e70043145eb6f7c3f3790
szakal <szak@gnuhost.eu>
2025-10-27 14:17:29 +0100

    Cookies Veryfy

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -7,3 +7,22 @@ def test_ing_cookie_consent():
             context = browser.new_context()
             page = context.new_page()
             page.goto("https://www.ing.pl")
+
+            page.get_by_role("button", name="Dostosuj").click()
+            page.get_by_role("switch", name="Cookies analityczne").locator("span").nth(1).click()
+            page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
+            cookies = context.cookies()
+
+            def get_cookie_by_name(cookies, name):
+                return next((cookie for cookie in cookies if cookie["name"] == name), None)
+
+            session_cookie = get_cookie_by_name(cookies, "cookiePolicyGDPR__details")
+            session_cookie2 = get_cookie_by_name(cookies, "cookiePolicyGDPR")
+            
+            assert session_cookie["name"] == "cookiePolicyGDPR__details", "Cookie name: 'cookiePolicyGDPR__details' ma niepoprawną wartość!"
+            assert session_cookie2["name"] == "cookiePolicyGDPR", "Cookie name: 'cookiePolicyGDPR' ma niepoprawną wartość!"
+            assert session_cookie2["value"] == "3", "Cookie 'cookiePolicyGDPR' ma niepoprawną wartość!"
+            assert "cookieCreateTimestamp" in session_cookie["value"], "Cookie 'cookiePolicyGDPR__details' ma niepoprawną wartość!"
+            page.wait_for_timeout(500)
+            print('Pozytywnie zweryfikowane cookies')
+            browser.close()
